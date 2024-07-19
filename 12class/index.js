b4ad95046041cc7296fe3b532b646000047ce37b
rajendra0968jangid <rajendra0968jangid@gmal.com>
2024-07-19 13:35:43 +0530

    twelve class

---
+++ 12class/index.js
@@ -0,0 +1,98 @@
+// let obj = {"full name":"rajendra","age":24}
+// console.log(obj.full name)
+// console.log(obj.age)
+// console.log(obj["full name"])
+
+
+// let arr = ["rajendra",24]
+// console.log(arr)
+// console.log(arr[0])
+// console.log(arr.length)
+
+
+
+// let num1 = 5
+// let num2 = "5"
+// let result = num1 === num2;
+// console.log(result)
+
+// let num1 = 5;
+// let num2 = 3;
+// let result = (num1 == num2)?"yes":"rajendra"
+// console.log(result)
+
+// ternary opr
+// let shyam = 24
+// let ram = 26
+// let rajendra = 28
+
+// let result = (shyam > ram && shyam > rajendra)?"shyam":(ram > rajendra)?"ram":"rajendra"
+// console.log(result)
+
+
+// increment or decrement opr
+// pre increment ++variable
+// post increment variable++
+
+// let num1 = 5
+// console.log(++num1)
+// console.log(num1++)
+// console.log(num1)
+
+// let num2 = 4
+// console.log(--num2)
+// console.log(num2--)
+// console.log(num2)
+
+// let num1 = 5
+// let num2 = 3
+// let result = num1++ + --num2 + num1-- + --num2
+// console.log(result)
+
+// loops
+
+//while loop
+// let i = 0;
+// while (i < 5) {
+//     console.log(i,"rajendra")
+//     i++
+// }
+
+//do while loop
+// let i=0
+// do{
+//     console.log(i,'rajendra')
+//     i++
+// }while(i<5);
+
+
+//for loop
+// for(let i=0;i<5;i++){
+//     console.log(i,"rajendra")
+// }
+
+// function
+// function add(myself) {//parameter
+//     console.log(myself);
+// }
+
+// let name = "rajendra"
+// let name1 = "hello"
+// let age  = 24
+// let mydetail = {name:"hello",name1:"rajendra",age:24}
+
+// add(mydetail)//arguments
+
+// let date = new Date()
+// console.log(date.getDate())
+// sunday, monday, tue,wed,thu,fri,sat
+// console.log(date.getDay())
+
+// events
+// click,hover,dblclick,key press,key pull
+
+let table = 2
+for(let i=1;i<11;i++){
+    // console.log(`${table}X${i}=${table*i}`)
+    console.log(table+"X"+i+"="+(table*i))
+}
